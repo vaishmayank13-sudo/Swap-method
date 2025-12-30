32fe47c1061c17e5f608c10e7f3099ca5c3e7afd
Mayank Vaish <vaishmayank13@gmail.com>
2025-12-30 22:02:48 +0530

    Swap

---
+++ Swap_Method.py
@@ -0,0 +1,14 @@
+x=12
+y=13
+print("x=",x)
+print("y=",y)
+
+temp=x # temp=12
+x=y # x=13
+# y=x this is the wrong method,because already  x=13
+# so we will make the "temp" and use it
+y=temp
+print("swape Method")
+print("x value is ",x)
+print("y value is ",y)
+
