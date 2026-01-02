0acc4f290ca7d37c1b0924940b54d838fea83c71
Mayank Vaish <vaishmayank13@gmail.com>
2026-01-02 13:57:13 +0530

    leap

--- Idenfy_leap_year.py
+++ Idenfy_leap_year.py
@@ -1,10 +1,10 @@
 year=int(input("Enter the Year: "))
 
 # There are two condition for identify the leap year
-if year%400==0:
+if (year%400==0):
     print("This year is leap year")
 
-elif year%4==0 and year%100!=0:
+elif (year%4==0) and (year%100!=0):
     print("This year is leap year")
 
 # if both condition are False than run this condition
